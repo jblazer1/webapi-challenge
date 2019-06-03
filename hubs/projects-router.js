28e574126ac2b5361dceeb77fd61113444bb5d40
JamesB <jblazer1@yahoo.com>
2019-06-03 14:24:03 -0700

    Add DELETE to Projects

--- hubs/projects-router.js
+++ hubs/projects-router.js
@@ -45,4 +45,19 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// DELETE ======================== DELETE ================================ DELETE =========================== DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await Projects.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "The Project has been nuked!" });
+    } else {
+      res.status(404).json({ message: "The Project could not be found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error removing the Project." });
+  }
+});
+
 module.exports = router;
