5dbdeca72aeb7b5b2b8c57b85e093d51d8dcc3f6
JamesB <jblazer1@yahoo.com>
2019-06-02 18:18:20 -0700

    Tried to do a GET but doesn't work

--- hubs/projects-router.js
+++ hubs/projects-router.js
@@ -4,4 +4,14 @@ const Projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const projects = await Projects.find(req.query);
+    res.status(200).json(projects);
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({ message: "Error retrieving the projects. " });
+  }
+});
+
 module.exports = router;
