2c598fe13131169dc02ff5d566956aba8855f539
JamesB <jblazer1@yahoo.com>
2019-06-03 17:47:12 -0700

    Add POST to actions

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const server = express();
 
 server.use(express.json());
 server.use("/api/projects", ProjectsRouter);
-server.use("/api/actions", ActionsRouter);
+server.use("/api/projects", ActionsRouter);
 
 server.get("/", (req, res) => {
   res.send("This is a test. This is only a test.");
--- hubs/actions-router.js
+++ hubs/actions-router.js
@@ -4,4 +4,18 @@ const Actions = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
+// POST =========================== POST ============================ POST ============================== POST
+router.post("/:id/actions", async (req, res) => {
+  const messageInfo = { ...req.body, project_id: req.params.id };
+
+  try {
+    const saved = await Actions.insert(messageInfo);
+    res.status(201).json(saved);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to save message", error });
+  }
+});
+
+// GET ============================ GET =============================== GET ================================ GET
+
 module.exports = router;
