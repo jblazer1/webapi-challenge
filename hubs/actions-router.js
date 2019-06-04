0be033d25ac862be49249719b5bfbaadfd77aaa0
JamesB <jblazer1@yahoo.com>
2019-06-03 17:55:12 -0700

    Add GET Actions from Project

--- hubs/actions-router.js
+++ hubs/actions-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Actions = require("../data/helpers/actionModel");
+const Projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
@@ -17,5 +18,19 @@ router.post("/:id/actions", async (req, res) => {
 });
 
 // GET ============================ GET =============================== GET ================================ GET
+router.get("/:id/actions", async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const actions = await Projects.getProjectActions(id);
+
+    if (actions.length) {
+      res.json(actions);
+    } else {
+      res.status(500).json({ error: "No actions for this Project" });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 module.exports = router;
